49c65969abebc0c8a98af0318e3077335bb03411
shanoo <shanu.ag@gmail.com>
2021-07-05 23:09:35 +0530

    done successfully

---
+++ src/com/JavaDemo/Main.java
@@ -0,0 +1,14 @@
+package com.JavaDemo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Main a = new Main();
+a.add();
+    }
+    public void add()
+    {
+        System.out.print("Hello IntelliJ");
+    }
+}
